a07ee631c7abee40b7e0bd606695b337e44e9bf8
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2017-08-26 10:35:33 +0200

    Basic playlist and track info listing  working

--- zptfyls.py
+++ zptfyls.py
@@ -1,4 +1,4 @@
-"""Usage: zptfyls.py OAUTHTOKEN
+"""Usage: zptfyls.py USERNAME OAUTHTOKEN
 
 Obtain the oauth token manually from spotify, e.g. by visiting ->
 https://developer.spotify.com/web-api/console/get-search-item/
@@ -12,10 +12,32 @@ if __name__ == '__main__':
     arguments = docopt(__doc__)
 
 token = arguments['OAUTHTOKEN']
-
-print("Spotift playlists with token: ", token)
+username = arguments['USERNAME']
 
 spotipy = spotipy.Spotify(auth=token)
-results = spotipy.current_user_playlists()
+result = spotipy.current_user_playlists()
 
-print(results)
+# items, i varje item: colloborative, href, id, images, name, tracks
+# limit: 50, next, previous, total
+
+def list_playlist(playlist):
+
+    print(playlist['name'] + ' (' + str(playlist['tracks']['total']) + ' tracks)')
+    print('')
+
+    if playlist['owner']['id'] != username:
+        return
+
+    trackItems = spotipy.user_playlist_tracks(username, playlist['id'])
+    for item in trackItems['items']:
+        print(item['track']['name'])
+
+    print('')
+
+for playlist in result['items']:
+    list_playlist(playlist)
+
+print('')
+print('Total no of playlists: ' + str(result['total']))
+print('Limit: ' + str(result['limit']))
+print('')
